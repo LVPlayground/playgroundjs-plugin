b0aa0e4c59706a014c6032f11efee2a08c33724c
Russell Krupke <russell@sa-mp.nl>
2015-10-19 22:01:40 +0100

    Add a convenience Promise type that merges the resolver and promise concepts

---
+++ src/playground/bindings/promise.cc
@@ -0,0 +1,60 @@
+// Copyright 2015 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+#include "playground/bindings/promise.h"
+
+#include "base/logging.h"
+
+namespace bindings {
+
+Promise::Promise()
+    : has_settled_(false) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::MaybeLocal<v8::Promise::Resolver> maybe_resolver = v8::Promise::Resolver::New(context);
+  if (maybe_resolver.IsEmpty()) {
+    LOG(ERROR) << "Unable to create a new promise: Resolver::New() failed.";
+    return;
+  }
+
+  resolver_.Reset(isolate, maybe_resolver.ToLocalChecked());
+}
+
+Promise::~Promise() {}
+
+v8::Local<v8::Promise> Promise::GetPromise() const {
+  DCHECK(!resolver_.IsEmpty());
+  return resolver_.Get(v8::Isolate::GetCurrent())->GetPromise();
+}
+
+bool Promise::ResolveInternal(v8::Local<v8::Value> value) {
+  if (has_settled_)
+    return false;
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::Maybe<bool> result = resolver_.Get(isolate)->Resolve(context, value);
+  if (result.IsNothing())
+    LOG(ERROR) << "Resolving the promise has failed.";
+
+  return has_settled_ = true;
+}
+
+bool Promise::RejectInternal(v8::Local<v8::Value> value) {
+  if (has_settled_)
+    return false;
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::Maybe<bool> result = resolver_.Get(isolate)->Reject(context, value);
+  if (result.IsNothing())
+    LOG(ERROR) << "Rejecting the promise has failed.";
+
+  return has_settled_ = true;
+}
+
+}  // namespace bindings
---
+++ src/playground/bindings/promise.h
@@ -0,0 +1,93 @@
+// Copyright 2015 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+#ifndef PLAYGROUND_BINDINGS_PROMISE_H_
+#define PLAYGROUND_BINDINGS_PROMISE_H_
+
+#include <string>
+
+#include <include/v8.h>
+
+namespace bindings {
+
+// Encapsulates functionality for a v8 Promise. Keep a reference to the Promise instance around
+// (we suggest using std::unique_ptr<> when possible) to access the Resolve() and Reject() methods,
+// and return GetPromise() to the script so that it can interact with the promise.
+class Promise {
+ public:
+  Promise();
+  ~Promise();
+
+  // Returns the Promise object the script can use to observe the operation.
+  v8::Local<v8::Promise> GetPromise() const;
+
+  // Returns whether the promise has settled. Further calls to Resolve() and Reject() will fail
+  // when this is the case, as a promise can only settle once.
+  bool HasSettled() const { return has_settled_; }
+
+  // Resolves the promise with |value|. The promise must not have been settled yet. The supported
+  // types are listed further down in this header file.
+  template<typename T> bool Resolve(T value);
+
+  // Rejects the promise with |value|. The promise must not have been settled yet. The supported
+  // types are listed further down in this header file.
+  template<typename T> bool Reject(T value);
+
+ private:
+  bool ResolveInternal(v8::Local<v8::Value> value);
+  bool RejectInternal(v8::Local<v8::Value> value);
+
+  v8::Global<v8::Promise::Resolver> resolver_;
+  bool has_settled_;
+};
+
+template<> bool Promise::Resolve(v8::Local<v8::Value> value) { return ResolveInternal(value); }
+template<> bool Promise::Reject(v8::Local<v8::Value> value) { return RejectInternal(value); }
+
+template<> bool Promise::Resolve(bool value) {
+  return ResolveInternal(v8::Boolean::New(v8::Isolate::GetCurrent(), value));
+}
+
+template<> bool Promise::Reject(bool value) {
+  return RejectInternal(v8::Boolean::New(v8::Isolate::GetCurrent(), value));
+}
+
+template<> bool Promise::Resolve(const std::string& value) {
+  v8::MaybeLocal<v8::String> string = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), value.c_str(), v8::NewStringType::kNormal);
+  if (string.IsEmpty())
+    return false;
+
+  return ResolveInternal(string.ToLocalChecked());
+}
+
+template<> bool Promise::Reject(const std::string& value) {
+  v8::MaybeLocal<v8::String> string = v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), value.c_str(), v8::NewStringType::kNormal);
+  if (string.IsEmpty())
+    return false;
+
+  return RejectInternal(string.ToLocalChecked());
+}
+
+#define DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(type) \
+  template<> bool Promise::Resolve(type value) { \
+    return ResolveInternal(v8::Number::New(v8::Isolate::GetCurrent(), static_cast<double>(value))); \
+  } \
+  template<> bool Promise::Reject(type value) { \
+    return RejectInternal(v8::Number::New(v8::Isolate::GetCurrent(), static_cast<double>(value))); \
+  }
+
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(char)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(unsigned char)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(short)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(unsigned short)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(int)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(unsigned int)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(long)
+DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE(unsigned long)
+
+#undef DEFINE_RESOLVE_REJECT_FOR_NUMBER_TYPE
+
+}  // namespace bindings
+
+#endif  // PLAYGROUND_BINDINGS_PROMISE_H_
