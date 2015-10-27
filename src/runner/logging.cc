7a63ed3b89933d61ff6391d2458da7051d7b2773
Russell Krupke <russell@sa-mp.nl>
2015-10-27 20:36:10 +0000

    The test runner should use playgroundjs dynamically rather than statically

---
+++ src/runner/logging.cc
@@ -0,0 +1,6 @@
+// Copyright 2015 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+// Include logging.cc to import the functionality in this binary.
+#include "playground/base/logging.cc"
