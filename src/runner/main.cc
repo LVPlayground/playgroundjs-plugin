522da82b30c8cb3cbac887fbf3a0c95c56b721c8
Russell Krupke <russell@sa-mp.nl>
2015-10-27 11:37:52 +0000

    Begin mocking a SA-MP server in the test runner
    We'd like to be able to run tests without an installation of the SA-MP
    server as well.

--- src/runner/main.cc
+++ src/runner/main.cc
@@ -2,10 +2,40 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+#include <stdarg.h>
 #include <stdio.h>
 
+#include "playground/base/logging.h"
+#include "playground/plugin/sdk/amx.h"
+#include "playground/plugin/sdk/plugincommon.h"
 #include "playground/test_runner.h"
 #include "playground/version.h"
+
+// Exports used by the SA-MP plugin runtime, which PlaygroundJS uses to initialize itself.
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData);
+PLUGIN_EXPORT void PLUGIN_CALL Unload();
+PLUGIN_EXPORT void PLUGIN_CALL ProcessTick();
+
+namespace {
+
+// Local implementation of the logprintf() function exported by the SA-MP server. Does not log the
+// issued statements to a file (not even to server_log.txt).
+void logprintf(char* format, ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  vprintf(format, arguments);
+  va_end(arguments);
+
+  putc('\n', stdout);
+}
+
+// The CallPublicFilterScript and CallPublicGameMode functions are not provided by this test
+// runner. Log an error message to the console instead.
+void UnimplementedCallPublicFunction(char* function_name) {
+  LOG(ERROR) << "Unimplemented method. Cannot invoke " << function_name << ".";
+}
+
+}  // namespace
 
 int main(int argc, char** argv) {
   printf("=== Las Venturas Playground v%d.%d.%d (v8 %d.%d.%d) ==========================\n\n",
@@ -16,8 +46,23 @@ int main(int argc, char** argv) {
   if (!playground::RunPlaygroundTests(argc, argv))
     return 1;
 
-  // TODO: Do something useful here.
-  printf("\n");
+  {
+    // TODO(Russell): Pass PLUGIN_DATA_AMX_EXPORTS in |plugin_data| with something sensible.
+    void* plugin_data[] = {
+      /* PLUGIN_DATA_LOGPRINTF */     logprintf,
+      /* PLUGIN_DATA_AMX_EXPORTS */   nullptr,
+      /* PLUGIN_DATA_CALLPUBLIC_FS */ UnimplementedCallPublicFunction,
+      /* PLUGIN_DATA_CALLPUBLIC_GM */ UnimplementedCallPublicFunction
+    };
+
+    Load(plugin_data);
+
+    // TODO(Russell): Fire the OnGameModeInit event so that PlaygroundJS initializes itself.
+    // TODO(Russell): Call ProcessTick() every few ms so that timers in PlaygroundJS work, as well
+    //                as internal functionality that depends on it.
+
+    Unload();
+  }
 
   return 0;
 }
