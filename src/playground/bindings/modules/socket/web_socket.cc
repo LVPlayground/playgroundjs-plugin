4d8cc847bfd2c9e125ceb7244825c7982f0ba18d
Russell <russell@sa-mp.nl>
2020-06-07 04:30:38 +0100

    WebSockets: Resolve DNS

--- src/playground/bindings/modules/socket/web_socket.cc
+++ src/playground/bindings/modules/socket/web_socket.cc
@@ -4,14 +4,53 @@
 
 #include "bindings/modules/socket/web_socket.h"
 
+#include "base/logging.h"
+#include "bindings/runtime.h"
+#include "bindings/utilities.h"
+
 namespace bindings {
 namespace socket {
 
-WebSocket::WebSocket() = default;
+namespace {
+
+std::shared_ptr<bindings::Runtime> Runtime() {
+  return bindings::Runtime::FromIsolate(v8::Isolate::GetCurrent());
+}
+
+}  // namespace
+
+WebSocket::WebSocket()
+    : main_thread_io_context_(Runtime()->main_thread_io_context()),
+      background_io_context_(Runtime()->background_io_context()),
+      resolver_(background_io_context_) {}
 
 WebSocket::~WebSocket() = default;
 
-void WebSocket::Open(SocketOpenOptions options, OpenCallback open_callback) {}
+void WebSocket::CallOnMainThread(boost::function<void()> function) {
+  main_thread_io_context_.post(function);
+}
+
+void WebSocket::Open(SocketOpenOptions options, OpenCallback open_callback) {
+  boost::asio::ip::tcp::resolver::query query(options.host, std::to_string(options.port));
+
+  resolver_.async_resolve(
+    query, boost::bind(&WebSocket::OnResolved, this, boost::asio::placeholders::error,
+                       boost::asio::placeholders::iterator, options, open_callback));
+}
+
+void WebSocket::OnResolved(const boost::system::error_code& ec,
+                           boost::asio::ip::tcp::resolver::iterator endpoint_iterator,
+                           SocketOpenOptions options,
+                           OpenCallback open_callback) {
+  if (ec) {
+    CallOnMainThread(boost::bind(open_callback, ec));
+    return;
+  }
+
+  boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
+
+  LOG(INFO) << __FUNCTION__;
+}
 
 void WebSocket::Read(ReadCallback read_callback, ErrorCallback error_callback) {}
 
--- src/playground/bindings/modules/socket/web_socket.h
+++ src/playground/bindings/modules/socket/web_socket.h
@@ -8,6 +8,9 @@
 #include "base/macros.h"
 #include "bindings/modules/socket/base_socket.h"
 
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
 namespace bindings {
 namespace socket {
 
@@ -25,6 +28,25 @@ class WebSocket : public BaseSocket {
   void Close(CloseCallback close_callback) override;
 
  private:
+  // Schedules the given |function| to be called on the main thread. Will be aligned with SA-MP
+  // server frames. This is necessary because Socket I/O is done on a background thread.
+  void CallOnMainThread(boost::function<void()> function);
+
+  // Called when DNS resolution has finished prior to opening the connection. We might now know
+  // where the connection has to be opened to.
+  void OnResolved(const boost::system::error_code& ec,
+                  boost::asio::ip::tcp::resolver::iterator endpoint_iterator,
+                  SocketOpenOptions options,
+                  OpenCallback open_callback);
+
+
+  // The IO Contexts, owned by the bindings Runtime, on which to post tasks.
+  boost::asio::io_context& main_thread_io_context_;
+  boost::asio::io_context& background_io_context_;
+
+  // The resolver used for resolving DNS, when used rather than an IP address.
+  boost::asio::ip::tcp::resolver resolver_;
+
   DISALLOW_COPY_AND_ASSIGN(WebSocket);
 };
 
